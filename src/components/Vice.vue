8d52757fbf1468f6f724bd5dd7cd4d4f32a5acf0
recurn <61036489+recurn@users.noreply.github.com>
2021-04-05 20:22:10 -0700

    Added best vice streak, total times commited

--- src/components/Vice.vue
+++ src/components/Vice.vue
@@ -6,9 +6,16 @@
     </div>
 
     <p>
+      <i class="pi pi-clock" />
       {{ vice.stats.timeSinceLast.days }}d {{ vice.stats.timeSinceLast.hours }}h
       {{ vice.stats.timeSinceLast.minutes }}m
     </p>
+    <p>
+      <img src="trophy.svg" alt="" style="width: 20px; height: 20px" />
+      {{ vice.best.days }}d {{ vice.best.hours }}h {{ vice.best.minutes }}m
+    </p>
+
+    <p><i class="pi pi-replay"></i>{{ vice.stats.history.length }}</p>
     <Button
       label="Reset"
       class="p-button-rounded"
@@ -35,28 +42,50 @@ export default {
     const emitter = internalInstance.appContext.config.globalProperties.emitter;
 
     onBeforeMount(() => {
+      let vice = props.vice;
+      let shouldUpdate = false;
+      let now = new Date();
+
+      let lastTimeNum = Date.parse(vice.stats.history[0].time);
+
+      let diff = now - lastTimeNum;
+
+      if (diff > vice.best.time) {
+        vice.best.time = diff;
+        shouldUpdate = true;
+      }
+
+     
+
       const { updateDoc } = useDocument(
         "users/" + props.user.uid + "/vices",
         props.vice.id
       );
-      let vice = props.vice;
+
       let days = vice.stats.timeSinceLast.days;
       if (vice.lastAward == null) {
         vice.lastAward = 0;
       }
+
       if (vice.lastAward < days) {
+        shouldUpdate = true;
         let xp = 0;
         for (let i = vice.lastAward + 1; i <= days; i++) {
           //maximum extra 200% reward after 20 days
-          console.log(i);
           xp += Math.round(25 * Math.min(15, 1 + i * 0.5));
         }
         emitter.emit("addXp", {
           xp: xp,
           message: `For abstaining from ${vice.name} for ${days} days`,
         });
+      }
+
+      if (shouldUpdate) {
         updateDoc({
           lastAward: days,
+          best: {
+            time: vice.best.time,
+          },
         });
       }
     });
@@ -67,5 +96,12 @@ export default {
 <style scoped>
 .p-button-rounded {
   margin-top: auto;
+}
+img {
+  margin: 5px;
+}
+i {
+  font-size: 20px;
+  margin: 5px;
 }
 </style>
